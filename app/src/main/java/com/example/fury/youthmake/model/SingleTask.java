ac1e623461be6ee06f0d821d1fe1e8f37cf6a622
flt <471163887@qq.com>
2015-12-06 22:17:44 +0800

    修改一周界面，添加one day class and one week class

--- app/src/main/java/com/example/fury/youthmake/model/SingleTask.java
+++ app/src/main/java/com/example/fury/youthmake/model/SingleTask.java
@@ -7,6 +7,74 @@ package com.example.fury.youthmake.model;
  * Auth: flt
  */
 public class SingleTask {
-    String description;
-    String prospect;
+
+    private boolean morning_finish; //早上任务完成标记
+    private boolean noon_finish;    //下午任务完成标记
+    private boolean night_finish;   //晚上任务完成标记
+
+    private boolean complish_tag;   //所有任务完成标记
+
+    private String morning_des;     //早上任务描述
+    private String noon_des;        //下午任务描述
+    private String night_des;       //晚上任务描述
+
+    public SingleTask(){
+
+    }
+
+    public boolean isComplish_tag() {
+        return complish_tag;
+    }
+
+    public void setComplish_tag(boolean complish_tag) {
+        this.complish_tag = complish_tag;
+    }
+
+    public String getMorning_des() {
+        return morning_des;
+    }
+
+    public void setMorning_des(String morning_des) {
+        this.morning_des = morning_des;
+    }
+
+    public boolean isMorning_finish() {
+        return morning_finish;
+    }
+
+    public void setMorning_finish(boolean morning_finish) {
+        this.morning_finish = morning_finish;
+    }
+
+    public String getNight_des() {
+        return night_des;
+    }
+
+    public void setNight_des(String night_des) {
+        this.night_des = night_des;
+    }
+
+    public boolean isNight_finish() {
+        return night_finish;
+    }
+
+    public void setNight_finish(boolean night_finish) {
+        this.night_finish = night_finish;
+    }
+
+    public String getNoon_des() {
+        return noon_des;
+    }
+
+    public void setNoon_des(String noon_des) {
+        this.noon_des = noon_des;
+    }
+
+    public boolean isNoon_finish() {
+        return noon_finish;
+    }
+
+    public void setNoon_finish(boolean noon_finish) {
+        this.noon_finish = noon_finish;
+    }
 }
--- app/src/main/java/com/example/fury/youthmake/model/WeekTask.java
+++ app/src/main/java/com/example/fury/youthmake/model/WeekTask.java
@@ -9,12 +9,18 @@ import android.util.Log;
  * Auth: flt
  */
 public class WeekTask {
-    private int ID;
-    private String StartTime;
-    private String endTime;
-    //public int TaskCount;
-    //boolean Acomplish = false;
-    //String[] TaskDescription;
+    private boolean W_finish;
+    private int W_ID;
+    private int W_taskNum;
+
+    private String W_startTime;
+    private String W_endTime;
+    private String W_description;
+
+    public WeekTask(){
+
+    }
+    //不能用单例，因为以后可能需要保存这些WeekTask对象
     /*private static WeekTask weekTask = null;
 
     public static WeekTask getInstance() {
@@ -23,11 +29,56 @@ public class WeekTask {
         }
         return weekTask;
     }*/
-    public WeekTask(){
 
-    }
     public void print(){
 
     }
 
+    public String getW_description() {
+        return W_description;
+    }
+
+    public void setW_description(String w_description) {
+        W_description = w_description;
+    }
+
+    public String getW_endTime() {
+        return W_endTime;
+    }
+
+    public void setW_endTime(String w_endTime) {
+        W_endTime = w_endTime;
+    }
+
+    public boolean isW_finish() {
+        return W_finish;
+    }
+
+    public void setW_finish(boolean w_finish) {
+        W_finish = w_finish;
+    }
+
+    public int getW_ID() {
+        return W_ID;
+    }
+
+    public void setW_ID(int w_ID) {
+        W_ID = w_ID;
+    }
+
+    public String getW_startTime() {
+        return W_startTime;
+    }
+
+    public void setW_startTime(String w_startTime) {
+        W_startTime = w_startTime;
+    }
+
+    public int getW_taskNum() {
+        return W_taskNum;
+    }
+
+    public void setW_taskNum(int w_taskNum) {
+        W_taskNum = w_taskNum;
+    }
 }
